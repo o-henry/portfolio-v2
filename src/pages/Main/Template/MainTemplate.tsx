55aa1824baa63a5d6c4a5ae9ae85a7e8bb6d21ce
Channing <cksgod16@gmail.com>
2020-04-04 12:47:17 +0900

    Add sass package and set reset/main/font scss

--- src/pages/Main/Template/MainTemplate.tsx
+++ src/pages/Main/Template/MainTemplate.tsx
@@ -10,13 +10,13 @@ function MainTemplate(): React.ReactElement {
             display: 'flex',
             flexWrap: 'wrap',
             '& > *': {
-                margin: theme.spacing(1),
+                // margin: theme.spacing(3),
                 width: theme.spacing(500),
-                height: theme.spacing(16),
+                height: theme.spacing(3),
             },
         },
         paper: {
-            padding: theme.spacing(50),
+            padding: theme.spacing(40),
             textAlign: 'center',
             color: theme.palette.text.secondary,
         },
@@ -26,7 +26,7 @@ function MainTemplate(): React.ReactElement {
 
     return (
         <div className={classes.root}>
-            <Grid container spacing={3}>
+            <Grid container spacing={5}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>xs=12</Paper>
                 </Grid>
@@ -35,18 +35,6 @@ function MainTemplate(): React.ReactElement {
                 </Grid>
                 <Grid item xs={6}>
                     <Paper className={classes.paper}>xs=6</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>xs=3</Paper>
                 </Grid>
             </Grid>
         </div>
